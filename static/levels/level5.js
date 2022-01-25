5bd06a50d64f1103d28f520cc21c8c084bcd5e73
Yuval Rosen <yuv.rosen@gmail.com>
2022-01-25 09:06:54 +0200

    remove betterfetch

--- static/levels/level5.js
+++ static/levels/level5.js
@@ -1,5 +1,5 @@
 async function streamingService() {
-    var server_response = await (await betterFetch('/level5_streaming')).json();
+    var server_response = await (await fetch('/level5_streaming')).json();
     if (server_response.success) {
         document.getElementById('stream').innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allow="picture-in-picture" allowfullscreen></iframe>`;
     }
--- static/utils.js
+++ static/utils.js
@@ -1,52 +1,10 @@
 // Ignore this file, just managing the level movememnt and stuff like that, nothing to do with the actual levels
 
-async function betterFetch(local_url) {
-    if (location.href.includes('127.0.0.1')) {
-        return await fetch(local_url);
-    } else {
-        return await fetch('https://odyssey-ctf.herokuapp.com' + local_url);
-    }
-}
-
-function setCookie(name, value, hours) {
-    var expires = "";
-    if (hours) {
-        var date = new Date();
-        date.setTime(date.getTime() + (hours*60*60*1000));
-        expires = "; Expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; Path=/";
-}
-
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i=0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
-function eraseCookie(name) {   
-    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-}
-
-
-const cookie_expiration_hours = 24;
-
-
 async function gotoLevel(event, level) {
     var pwd = event.target.elements.password.value;
 
-    var server_response = await (await betterFetch('/check_level?level=' + level + '&password=' + pwd)).json();
+    var server_response = await (await fetch('/check_level?level=' + level + '&password=' + pwd)).json();
     if (server_response.success) {
-        // save password in cookies:
-        // let current_passwords = JSON.parse(getCookie('passwords') || '{}');
-        // current_passwords[level] = pwd;
-        // setCookie('passwords', JSON.stringify(current_passwords), cookie_expiration_hours);
-        
         location.href = '/levels/' + level;
     } else {
         alert(server_response.message);
