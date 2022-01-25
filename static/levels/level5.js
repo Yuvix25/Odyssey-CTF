d23731df2eb8d5d41efe13baf06ad21f011893d1
Yuval Rosen <yuv.rosen@gmail.com>
2022-01-25 09:11:14 +0200

    add password to level5 sol

--- static/levels/level5.js
+++ static/levels/level5.js
@@ -1,9 +1,7 @@
 async function streamingService() {
     var server_response = await (await fetch('/level5_streaming')).json();
     if (server_response.success) {
-        document.getElementById('stream').innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allow="picture-in-picture" allowfullscreen></iframe>`;
+        document.getElementById('stream').innerHTML = `<iframe width="560" height="315" src="${server_response.url}" frameborder="0" allow="picture-in-picture" allowfullscreen></iframe>`;
     }
-    else {
-        document.getElementById('message').innerHTML = server_response.message;
-    }
+    document.getElementById('message').innerHTML = server_response.message;
 }
